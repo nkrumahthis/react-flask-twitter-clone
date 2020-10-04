39cae0170529454a8384bf89bad49446628c69fb
nkrumahthis <inkusswamp@gmail.com>
2020-10-04 14:08:49 +0000

    created a basc flask project

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///twitter.db"
+
+# DB
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column('student_id', db.Integer, primary_key = True)
+    username = db.Column(db.String(24))
+    email = db.Column(db.String(64))
+    pwd = db.Column(db.String(64))
+
+    # Constructor
+    def __init__(self, username, email, pwd):
+        self.username = username
+        self.email = email
+        self.pwd = pwd
 
 @app.route('/')
+
 def index():
-    return "Hello, World"
+    db.create_all()
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(debug = True)
